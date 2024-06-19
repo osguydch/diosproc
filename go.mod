bc76c97e53ea457020fb353919cc44d70c35fc00
osguydch <27235294@qq.com>
2024-06-19 13:43:32 +0800

    rebuild diosproc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module diosproc
+
+go 1.22.1
---
+++ server/main.go
@@ -0,0 +1,99 @@
+package main
+
+/*
+int Add(int a, int b){
+    return a+b;
+}
+*/
+import "C"
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "net"
+    // importing generated stubs
+    gen "github.com/osguydch/diosproc/proto/device/V3"
+    "google.golang.org/grpc"
+)
+// DeviceServerImpl will implement the service defined in protocol buffer definitions
+type DeviceServerImpl struct {
+    gen.UnimplementedDeviceServer
+}
+
+// Open is the implementation of RPC call defined in protocol definitions.
+// This will take OpenRequest message and return OpenReply
+func (g *DeviceServerImpl) Open(ctx context.Context, request *gen.OpenRequest) (*gen.OpenReply, error) {
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("hello %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Close( ctx context.Context, request *gen.OpenRequest) (*gen.CloseReply, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("hello %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Get( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Get %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Update( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Update %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Add( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Add %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Del( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Del %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Action( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Action %s",request.Name),
+    },nil
+}
+
+func (g *DeviceServerImpl) Message( ctx context.Context, request *gen.DoRequest) (*gen.DoResponse, error)
+{
+    return &gen.OpenReply{
+        Message: fmt.Sprintf("Message %s",request.Name),
+    },nil
+}
+
+
+
+func main() {
+    // create new gRPC server
+    fmt.Println(C.add(C.int(20), C.int(15)));
+
+    server := grpc.NewServer()
+    // register the GreeterServerImpl on the gRPC server
+    gen.RegisterDeviceServer(server, &DeviceServerImpl{})
+    // start listening on port :8080 for a tcp connection
+    if l, err := net.Listen("tcp", ":8080"); err != nil {
+        log.Fatal("error in listening on port :8080", err)
+    } else {
+        // the gRPC server
+        if err:=server.Serve(l);err!=nil {
+            log.Fatal("unable to start server",err)
+        }
+    }
+}
